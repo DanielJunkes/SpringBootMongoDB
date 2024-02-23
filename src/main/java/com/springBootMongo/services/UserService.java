422e8d22696ac566b3c4e5b1960be50e9bf33783
DanielJunkes <danielmafioleti2@gmail.com>
2024-02-22 21:34:27 -0300

    Insert de user

--- src/main/java/com/springBootMongo/services/UserService.java
+++ src/main/java/com/springBootMongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springBootMongo.domain.User;
+import com.springBootMongo.dto.UserDTO;
 import com.springBootMongo.respository.UserRepository;
 import com.springBootMongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User Insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
